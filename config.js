11687ff8f9881bc2002f2a5fc14f71889fc5bfc7
Ifunanya Ikemma <ifavour902@gmail.com>
2017-11-22 08:56:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         'tiDAqHKLIpkUlmvHc4kZ6M9X5',
-  consumer_secret:      'meEpBs0pElbEPn6yi9sxRD5Bi2mfut8fYuNwp79OarMX81RJFw',
-  access_token:         '2180729507-HOIEhGHysrDVMoDzHQ8pDY5ruDaeUv3lXXQoD2N',
-  access_token_secret:  'j8i750XiIJQpCMXdYH5PO2vRxqzsBEd9Q8LXgHyCQtIup',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
